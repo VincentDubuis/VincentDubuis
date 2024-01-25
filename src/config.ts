a6b3232d306cf93ce2d03c52d9502bc115bb4f57
Vincent Dubuis <vincent.dubuis@heig-vd.ch>
2024-01-25 09:39:59 +0100

    Ajout du fichier de config de deploiement

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  website: "https://VincentDubuis.github.io/", // replace this with your deployed domain
   author: "Sat Naing",
   desc: "Site de veille technologique de Vincent Dubuis",
   title: "Dubuis Vincent - LabVT",
